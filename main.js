7c4cc457a7ce70a1e6ae819aa2b787fdfa5ab38f
Jheniffer Farias <jhenifferfarias.dev@gmail.com>
2023-02-06 15:11:00 -0300

    event - get element by id

---
+++ main.js
@@ -0,0 +1,7 @@
+const nameParagrafo = document.getElementById('name');
+nameParagrafo.addEventListener('click', changeName);
+
+function changeName(event) {
+    nameParagrafo.innerHTML= 'clicou';
+}
+
