ecd836679b5b5f798ea4f5b511f2dc4778e8eba6
Jheniffer Farias <jhenifferfarias.dev@gmail.com>
2023-02-06 17:45:16 -0300

    event - get element by class name

--- main.js
+++ main.js
@@ -1,7 +1,30 @@
+// get element by id
+
 const nameParagrafo = document.getElementById('name');
 nameParagrafo.addEventListener('click', changeName);
 
-function changeName(event) {
-    nameParagrafo.innerHTML= 'clicou';
-}
+function changeName() {
+    nameParagrafo.innerHTML= 'Agora clique nos nomes abaixo:';
+};
+
+// class name
+
+const items = Array.from(document.getElementsByClassName('item'));
+
+items.forEach(item => {
+    item.addEventListener('click', onItemClick);
+});
+
+function onItemClick(event) {
+    // alert('clicou', event.target.attributes.itemtype.value);
+    const value = event.target.attributes.itemtype.value;    
+
+    if (value === 'contato')
+    alert('Parabéns, você conseguiu!');
+    else(
+    alert('Ok, clique no próximo'));
+};
+
+
+
 
